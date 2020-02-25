8f350977fa62d5d0b4ff333e7e4cd87796443b53
Hiter Harris <hiterharris@comcast.net>
2020-02-25 17:17:34 -0500

    working on cookies and session

--- api/api-router.js
+++ api/api-router.js
@@ -9,17 +9,9 @@ router.use("/auth", authRouter);
 router.use("/users", restricted, usersRouter);
 
 router.get("/hash", (req, res) => {
-  // read the Authentication header
   const authentication = req.headers.authentication;
-
-  // hash the value from that header
   const hash = bcrypt.hashSync(authentication, 8);
-
   res.json({ originalValue: authentication, hashedValue: hash });
-
-  // $2a$13$9kEnIDU/c2mGBQq4R6u2eeCcpLBwZOYQXpdnTrezJeBWpJW8D/jDa
-  // $2a$13$anVsq35CZDmj.Fg8LDn6be0WdcJ6EOlLyY0AddqAbuov7EuP/uUIC
-  // $2a$08$zLoclERJ2mulEzJ2q5/sYutJtHDmQrLdZFsrLdeTMCmHwlNBuQCbu
 });
 
 router.get("/", (req, res) => {
--- api/configure-middleware.js
+++ api/configure-middleware.js
@@ -1,9 +1,32 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const session = require('express-session');
+const KnexStore = require('connect-session-knex')(session);
+const knex = require('../database/dbConfig');
+
+const sessionConfig = {
+  name: 'cookie',
+  secret: 'keep it secret, keep it safe',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    maxAge: 1000 * 60 * 10,
+    secure: false,
+    httpOnly: true,
+  },
+  store: new KnexStore({
+    knex,
+    tablename: 'sessions',
+    createtable: true,
+    sidfieldname: 'sid',
+    clearInterval: 1000 * 60 * 10,
+  }),
+}
 
 module.exports = server => {
   server.use(helmet());
   server.use(express.json());
   server.use(cors());
+  server.use(session(sessionConfig));
 };
