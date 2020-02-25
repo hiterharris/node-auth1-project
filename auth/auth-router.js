6900d6b6619dc5a01c12634a09418d4d5e029e36
Hiter Harris <hiterharris@comcast.net>
2020-02-24 19:01:22 -0500

    removed comments

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,4 +1,4 @@
-const bcrypt = require("bcryptjs"); // <<<<<<<<<<<<<<<< npm i bcryptjs
+const bcrypt = require("bcryptjs");
 const router = require("express").Router();
 
 const Users = require("../users/users-model.js");
@@ -6,9 +6,9 @@ const Users = require("../users/users-model.js");
 router.post("/register", (req, res) => {
   let user = req.body;
 
-  const hash = bcrypt.hashSync(user.password, 8); // << 1
+  const hash = bcrypt.hashSync(user.password, 8);
 
-  user.password = hash; // <<<<<<<<<<<<<<<<<<<<<<<<<<<< 2
+  user.password = hash;
 
   Users.add(user)
     .then(saved => {
